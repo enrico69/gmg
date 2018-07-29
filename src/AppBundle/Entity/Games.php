4375a6b4f13a6ad4bb534581c7a67b2b0b4c6352
Eric COURTIAL <e.courtial30@gmail.com>
2018-07-29 16:46:59 -0400

    Created entity and repo

---
+++ src/AppBundle/Entity/Games.php
@@ -0,0 +1,248 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Games
+ *
+ * @ORM\Table(name="games")
+ * @ORM\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\GamesRepository")
+ */
+class Games
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=false)
+     */
+    protected $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="platform", type="string", length=255, nullable=false)
+     */
+    protected $platform;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="to_play_solo", type="boolean", nullable=false)
+     */
+    protected $toPlaySolo = '0';
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="to_play_multi", type="boolean", nullable=false)
+     */
+    protected $toPlayMulti = '0';
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="copy", type="boolean", nullable=false)
+     */
+    protected $copy = '0';
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="many", type="boolean", nullable=false)
+     */
+    protected $many;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="top_game", type="boolean", nullable=false)
+     */
+    protected $topGame = '0';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="comments", type="text", length=65535, nullable=false)
+     */
+    protected $comments;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="to_do", type="boolean", nullable=false)
+     */
+    protected $toDo = '0';
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPlatform()
+    {
+        return $this->platform;
+    }
+
+    /**
+     * @param string $platform
+     */
+    public function setPlatform($platform)
+    {
+        $this->platform = $platform;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isToPlaySolo()
+    {
+        return $this->toPlaySolo;
+    }
+
+    /**
+     * @param bool $toPlaySolo
+     */
+    public function setToPlaySolo($toPlaySolo)
+    {
+        $this->toPlaySolo = $toPlaySolo;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isToPlayMulti()
+    {
+        return $this->toPlayMulti;
+    }
+
+    /**
+     * @param bool $toPlayMulti
+     */
+    public function setToPlayMulti($toPlayMulti)
+    {
+        $this->toPlayMulti = $toPlayMulti;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isCopy()
+    {
+        return $this->copy;
+    }
+
+    /**
+     * @param bool $copy
+     */
+    public function setCopy($copy)
+    {
+        $this->copy = $copy;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isMany()
+    {
+        return $this->many;
+    }
+
+    /**
+     * @param bool $many
+     */
+    public function setMany($many)
+    {
+        $this->many = $many;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isTopGame()
+    {
+        return $this->topGame;
+    }
+
+    /**
+     * @param bool $topGame
+     */
+    public function setTopGame($topGame)
+    {
+        $this->topGame = $topGame;
+    }
+
+    /**
+     * @return string
+     */
+    public function getComments()
+    {
+        return $this->comments;
+    }
+
+    /**
+     * @param string $comments
+     */
+    public function setComments($comments)
+    {
+        $this->comments = $comments;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isToDo()
+    {
+        return $this->toDo;
+    }
+
+    /**
+     * @param bool $toDo
+     */
+    public function setToDo($toDo)
+    {
+        $this->toDo = $toDo;
+    }
+}
+
---
+++ src/AppBundle/Entity/GamesRepository.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by Eric COURTIAL.
+ * @author <e.courtial30@gmail.com>
+ * Date: 18-07-29
+ */
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class GamesRepository
+ * @package AppBundle\Entity
+ */
+class GamesRepository extends EntityRepository
+{
+
+}
