fbec2916ec3e8757ab8a3afc7fbf273dad9bde7a
Eric COURTIAL <e.courtial30@gmail.com>
2018-09-02 14:43:41 -0400

    Added FOS User Bundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author     Eric COURTIAL <ecourtial@absolunet.com>
+ * @copyright  Copyright (c) 2018 Absolunet (http://www.absolunet.com)
+ * @link       http://www.absolunet.com
+ */
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class User
+ * @package AppBundle\Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+}
